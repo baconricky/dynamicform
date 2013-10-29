f4565224fd027ed3bfcdf84ebe0b24cd5632ce6d
Nick Harris <nharris@redhat.com>
2013-10-29 17:42:17 -0400

    removed extra files

--- js/jquery.gatedform.settings.stage.js
+++
@@ -1,7 +0,0 @@
-var DynamicForm = window.DynamicForm = window.DynamicForm || {};
-
-DynamicForm.ExtSettings = {
-    debug: false,
-    URL_PREFIX: (document.location.protocol === 'https:' ? 'https://' : 'http://') + 'stage.redhat.com',
-    QA_Version: "1.8.7 STAGE",
-};
